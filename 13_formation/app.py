deed3c6e0dad9789d8ed28c8c98d1dd8d1f3b4e2
denchen11214 <dchen22@stuy.edu>
2018-09-30 17:34:36 -0400

    put comments and made it so that either the get or post method is used

--- 13_formation/app.py
+++ 13_formation/app.py
@@ -4,14 +4,20 @@
 #9/27/18
 
 from flask import Flask, render_template, request
+import random #used to choose btwn get and post
 app = Flask(__name__)
 
 @app.route("/")
-def sth():
-    print(app)
-    return render_template('echo.html')
-@app.route('/auth')
+def home():
+    return render_template('echo.html', method = random.choice(["GET","POST"]))# randomly chooses a request method
+@app.route('/auth', methods = ["GET","POST"])
 def authenticate():
-    return render_template('auth.html', method = request.method, name = request.args['name'], wa = "WAAAAAAAAAAAAAAAAAAAAAAAAHHHAHAHAHHAHAHAH")
+    #request method and username stored
+    requestM = request.method
+    if requestM == "GET":
+        username = request.args["username"]
+    if requestM == "POST":
+        username = request.form["username"]
+    return render_template('auth.html', method = requestM, name = username, wa = "WAAAAAAAAAAAAAAAAAAAAAAAAHHHAHAHAHHAHAHAH")#template rendered using stored variables
 if __name__ == '__main__':
     app.run(debug=True)
