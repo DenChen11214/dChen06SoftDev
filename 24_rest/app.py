e3baa6a912a9547ec45ea59860d2fb9af2d8ce3d
denchen11214 <dchen22@stuy.edu>
2018-11-13 17:51:56 -0500

    obtained url, used JSON to make it a dictionary, and loaded the image to a html file

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -4,13 +4,19 @@
 #Date: 11/3/18
 
 from flask import Flask, render_template
-import urllib
+import urllib.request
+import json
 app = Flask(__name__)
 
 
 @app.route("/")
 def rest():
-    url = urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=tSOmupc7rttYZViprGK30TqKeqj23UqAbhFEukqN")
-    print(url)
+    url = urllib.request.urlopen("https://api.nasa.gov/planetary/earth/imagery/?lon=100.75&lat=1.5&date=2014-02-01&cloud_score=True&api_key=c9f1tityqeXECe3ENg25UCnIWGbr5s26fDeuSzR9")
+    info = url.read()
+    data = json.loads(info.decode("utf-8"))
+    #print(data)
+    image = data['url']
+    #print(image)
+    return render_template("index.html", link = image)
 if __name__ == '__main__':
     app.run(debug=True)
