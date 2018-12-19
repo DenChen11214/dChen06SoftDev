7d7daadf9115b6b4d9176ccdc4b8e7e815d39512
DenChen11214 <dchen22@stuy.edu>
2018-12-19 12:01:49 -0500

    worked on printing to conosle

--- 28_js/funky_town.js
+++ 28_js/funky_town.js
@@ -16,19 +16,29 @@ var gcd = function(a,b) {
     if (a > b) {
 	for (i = b; i > 0; i--) {
 	    if ((a%i==0) && (b%i==0)) {
-		return i;
+		console.log(i);
+		break
 	    }
 	}
     }
     else {
 	for (i = a; i > 0; i--) {
 	    if ((a%i==0) && (b%i==0)) {
-		return i;
+		console.log(i);
+		break
 	    }
 	}
     }
 }
 var students = ['joe','bob','mary','addison','dennis','andrew','tyler','jessica','karen','vincent','allen']
 var randomStudent = function(){
-    return students[parseInt(Math.random() * students.length)];
+    console.log(students[parseInt(Math.random() * students.length)]);
+}
+f = document.getElementById('fibb')
+g = document.getElementById('gcd')
+r = document.getElementById('randomS')
+r.addEventListener('click',randomStudent)
+g.addEventListener('click',gcd(4,8))
+var displayFibb = function(){
+    console.log(fibonacci(5))
 }
