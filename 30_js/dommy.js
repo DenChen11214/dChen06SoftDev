9f0cbb79e33380dbe3f68aba11f11505d7176643
denchen11214 <dchen22@stuy.edu>
2018-12-20 20:47:00 -0500

    changed code to fit the keys2success

--- 30_js/dommy.js
+++ 30_js/dommy.js
@@ -1,27 +1,66 @@
 /* Team SmallFish - Addison Huang and Dennis Chen
-   SoftDev1 pd6
-   K30 -- Sequential Progression III: Season of the Witch
-   2018-12-20*/
+SoftDev1 pd6
+K30 -- Sequential Progression III: Season of the Witch
+2018-12-20*/
 
 var fibonacci = function(n) {
-    var sum = [0,1];
-    for (var i = 2; i <= n; i++) {
-	sum.push(sum[i-2] + sum[i-1]);
-    }
-    return sum[sum.length - 1];
+  var sum = [0,1];
+  for (var i = 2; i <= n; i++) {
+    sum.push(sum[i-2] + sum[i-1]);
+  }
+  return sum[sum.length - 1];
 };
-var fib = 1
 
-var create = document.getElementById('b');
-create.addEventListener('click',function(){
-    var lis = document.getElementById('thelist');
-    var item = document.createElement('li');
+// var changeHeading = function(e) {
+//     var h = document.getElementById("h")
+//     h.innerHTML =
+// };
+
+// var removeItem = function(e) {
+// };
+
+// var lis = document.getElementsByTagName("li");
+//
+// for(var i=0; i < lis.length; i++) {
+//     lis[i].addEventListener('mouseover', );
+//     lis[i].addEventListener('mouseout', );
+//     lis[i].addEventListener('click', );
+// };
+var addItem = function(e) {
+    var list = document.getElementById('thelist');
+    var item = document.createElement("li");
     item.innerHTML = 'WORD';
-    lis.appendChild(item);});
-var fibBut = document.getElementById('fb');
-fibBut.addEventListener('click',function(){
+    list.appendChild(item);
+};
+
+var button = document.getElementById("b");
+button.addEventListener('click', addItem);
+
+var fib = function(n) {
+    if(n < 2){
+        return 1;
+    } else {
+        return fib(n-1) + fib(n-2);
+    }
+};
+var fibIndex = 1
+var addFib = function(e){
     var lis = document.getElementById('fiblist');
     var item = document.createElement('li');
-    item.innerHTML = fibonacci(fib);
-    fib += 1;
-    lis.appendChild(item);});
+    item.innerHTML = fib(fibIndex);
+    fibIndex += 1;
+    lis.appendChild(item);
+    console.log(e);
+};
+
+var addFib2 = function(e){
+    var lis = document.getElementById('fiblist');
+    var item = document.createElement('li');
+    item.innerHTML = fibonacci(fibIndex);
+    fibIndex += 1;
+    lis.appendChild(item);
+    console.log(e);
+};
+
+var fb = document.getElementById("fb");
+fb.addEventListener("click", addFib);
